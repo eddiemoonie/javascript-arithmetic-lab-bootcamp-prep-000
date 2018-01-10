537dbd9ed9d00e873b749f5467755ddbc1c74959
Ki Moon <eddiemoonie@gmail.com>
2018-01-10 01:28:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  var number = a + b;
+  return number;
+}
+
+function multiply(a, b) {
+  var number = a * b;
+  return number;
+}
+
+function subtract(a, b) {
+  var number = a - b;
+  return number;
+}
+
+function divide(a, b) {
+  var number = a / b;
+  return number;
+}
+
+function inc(n) {
+  var a = 1;
+  var number = a + n;
+  return number;
+}
+
+function dec(n) {
+  var a = 1;
+  var number = a - n;
+  return number;
+}
